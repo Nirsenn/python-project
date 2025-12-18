d0af684089225c6ab665443e8bdfd0f197f83816
Nirsenn <148534589+Nirsenn@users.noreply.github.com>
2025-12-18 11:35:11 +0100

    Update Corpus.py
    version finale

--- Corpus.py
+++ Corpus.py
@@ -1,12 +1,8 @@
 import pandas as pd
-from Document import Document
+from Document import Document, RedditDocument, ArxivDocument
 from Author import Author
 from datetime import datetime
 import re
-from collections import Counter
-import numpy as np
-from scipy.sparse import csr_matrix
-import math #calcul du logarithme pour la matrice IDF
 
 # Definition du singleton
 def singleton(cls):
@@ -18,7 +14,7 @@ def singleton(cls):
     return wrapper
 
 
-@singleton   #(Decorateur)
+#@singleton   #(Decorateur)
 class Corpus:
     def __init__(self, nom, id2aut, id2doc):
         self.nom = nom
@@ -26,20 +22,18 @@ class Corpus:
         self.id2doc = id2doc
         self.ndoc = len(id2doc)
         self.naut = len(self.authors)
-    
+
     def add_document(self, doc_id, doc):
         #document = Document(titre, auteur, date, url, texte) #ne pas créer un document mais prendre un objet document directement en entrée?
         self.id2doc[doc_id] = doc
         if self.id2doc[doc_id].auteur not in self.authors.keys():
             self.authors[self.id2doc[doc_id].auteur] = Author(self.id2doc[doc_id].auteur).add(doc_id, doc)
-    
 
     def afficher_par_date(self, n):
         docs = sorted(self.id2doc.values(), key=lambda d: d.date, reverse=True)
         print(f"Tri par date :")
         for doc in docs[:n]:
             print(f"- {doc.date} | {doc.titre} ({doc.auteur})")
-
 
     def afficher_par_titre(self, n):
         docs = sorted(self.id2doc.values(), key=lambda d: d.titre.lower())
@@ -50,11 +44,10 @@ class Corpus:
     def __repr__(self):
         return f"Corpus '{self.nom}' : {self.ndoc} documents, {self.naut} auteurs"
 
-        
     def to_dataframe(self):
         data = []
         for doc_id, doc in self.id2doc.items():
-            data.append({
+            row = {
                 'id' : doc_id,
                 'titre' : doc.titre,
                 'auteur' : doc.auteur,
@@ -63,87 +56,74 @@ class Corpus:
                 'texte' : doc.texte,
                 'taille_texte' : len(doc.texte),
                 'type' : doc.type
-            })
+            }
+
+            if isinstance(doc, RedditDocument):
+                row['nb_comment'] = doc.nb_comment
+            elif isinstance(doc, ArxivDocument):
+                row['coAuteur'] = doc.coAuteur
+            data.append(row)
+
         return pd.DataFrame(data)
 
+    # Sauvegarde du Corpus au format csv
     def save(self, filename):
         df = self.to_dataframe()
-        df.to_csv(filename, index=False)
+        df.to_csv(filename, index=False, sep="\t", encoding="utf-8")
 
+    # Charger un document csv en objet corpus
     def load(self, filename):
-        df = pd.read_csv(filename)
+        df = pd.read_csv(filename, sep="\t")
         for _, row in df.iterrows():
             doc = Document(row['titre'], row['auteur'], datetime.strptime(row['date'], "%Y-%m-%d"), row['url'], row['texte'])
             self.add_document(row['id'], doc)
         return self
     
+    # Affiche le passage d'un mot dans les textes
     def search(self, texte, mot):
         c = 1
-        regex = r"\b" + mot + r"\b"                                                             #pattern du mot à trouver
-        passages = re.finditer(regex, texte)
-        for passage in passages:                                                                #pour chaque passage trouvé
-            debut = passage.start() - 30                                                        #on prend un extrait de texte débutant 30 caractères avant et finissant 30 caracères après le mot
-            fin = passage.end() + 30
-            if debut < 0:                                                                       #si l'indice de début est négatif on le remet à 0
-                debut = 0
-            if fin >= len(texte):                                                               #si l'indice de fin est plus grand que la chaine, on le met à la fin de celle ci
-                fin = len(texte) - 1
+        regex = r"\b" + re.escape(mot) + r"\b"                                                             #pattern du mot à trouver
+        for passage in re.finditer(regex, texte):                                                                #pour chaque passage trouvé
+            debut = max(passage.start() - 30, 0)                                                       #on prend un extrait de texte débutant 30 caractères avant et finissant 30 caracères après le mot
+            fin = min(passage.end() + 30, len(texte))
             print(c, ":", texte[debut:fin])                                                     #affichage de chaque passage correspondant
-            c+=1
+            c += 1
         return c
     
+    # Affiche le passage d'un mot dans les textes sous forme de dataframe
     def concorde(self, texte, mot, taille):
-        i = 0
-        regex = r"\b" + mot + r"\b"                                                             #pattern à trouver (le mot en paramètre exclusivement)
-        passages = re.finditer(regex, texte)                                                    #stocker les indices de toutes les occurences du mot dans le texte
-        df = pd.DataFrame(columns=["contexte gauche", "motif trouvé", "contexte droit"])        #création du Dataframe
-        for passage in passages:                                                                #Pour chaque occurence du mot...
-            strt = "..."
-            end = "..."
-            debut = [(passage.start() - taille), (passage.start()-1)]                           #le contexte gauche commence taille caractères avant le début du mot et se termine 1 caractère avant
-            fin = [(passage.end() + 1), (passage.end() + taille)]                               #le contexte droit commence 1 caractère après la fin du mot et se termine taille caractères après
-            if debut[0] < 0:                                                                    #si l'indice du contexte gauche est plus petit que 0 (sors de la chaine) ont le met à 0
-                debut[0] = 0                                                                   
-                strt = ""                                                                        #et on ne met pas de "..." car il n'y a rien avant
-            if debut[1] < 0:
-                debut[1] = 0
-                strt = ""
-            if fin[1] >= len(texte):                                                            #si l'indice de din du contexte droit est plus grand que la taille de la chaine alors il se termine à la fin de celle ci
-                fin[1] = len(texte) - 1
-                end = ""                                                                        #et on ne met pas de "..." car il n'y a rien après
-            if fin[0] >= len(texte):
-                fin[1] = len(texte) - 1
-                end = ""
-            gauche = strt + texte[debut[0]:debut[1]]   
-            droite = texte[fin[0]:fin[1]] + end
-            df.loc[i] = pd.Series({"contexte gauche" : gauche, "motif trouvé": mot, "contexte droit": droite})  #ajout de la ligne dans le dataframe
-            i+=1
-        pd.set_option('display.width', 200)                                                     #paramètre pour que la dataframe s'affiche sur toute la longeur du terminal lors du print
+        regex = r"\b" + re.escape(mot) + r"\b"                                                             #pattern à trouver (le mot en paramètre exclusivement)
+        passages = []                                                   #stocker les indices de toutes les occurences du mot dans le texte
+        for passage in re.finditer(regex, texte):                                                                #Pour chaque occurence du mot...
+            debut = max(passage.start() - taille, 0)                        #le contexte gauche commence taille caractères avant le début du mot et se termine 1 caractère avant
+            fin = min(passage.end() + taille, len(texte))                              #le contexte droit commence 1 caractère après la fin du mot et se termine taille caractères après
+            gauche = texte[debut:passage.start()]  
+            if debut > 0:   # si on n’est pas au tout début
+                gauche = "..." + gauche
+            droite = texte[passage.end():fin] # contexte droit
+            if fin < len(texte):   # si on n’est pas à la toute fin
+                droite = droite + "..."
+            passages.append([gauche, passage.group(), droite])
+        df = pd.DataFrame(passages, columns=["contexte gauche", "motif trouvé", "contexte droit"])        #création du Dataframe
         print(df)
 
-
+    # Nettoie le texte pour le rendre exploitable
     def nettoyer_texte(self, texte):
-
         texte = texte.lower()                       # Mise en minuscules
-
         texte = texte.replace("\n", " ")            # Suppression des retours ligne
-
         texte = re.sub(r"[^\w\s]", " ", texte)      # Suppression de la ponctuation tout sauf lettres/chiffres/underscore/espaces
-        
         texte = re.sub(r"\d+", " ", texte)          # Suppression de la ponctuation des chiffres 
-
         texte = re.sub(r"\s+", " ", texte).strip()   # Suppression des espaces multiples
-
         return texte
 
-
+    # Affiche les mots les plus fréquents
     def stats(self, n=10):
         data = self.freq_mots(n)                               # Récupère le vocabulaire du Corpus et les données sous forme de Dataframe
         print("Nombre de mots différents :", len(data[0]))     # Nombre de mots différents = longueur du vocabulaire
         print(f"Les {n} mots les plus fréquents :")
         print(data[1].head(n))                                 # Affichage des n premiers éléments du dataframe trié
 
-
+    # Compte la fréquence de tous les mots dans le corpus, par terme puis par documents
     def freq_mots(self, n=10):
         mot_id = 1
         vocabulaire = {}                                        # Dictionnaire de forme {mot : {id : x, occurrences : y, docFreq : z}}
